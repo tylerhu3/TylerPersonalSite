d1633cac74adece23f455e19734c95eac95bfec8
tylerhu3 <tylerhu@yahoo.com>
2019-09-19 04:56:37 -0700

    so sick of working on mobile landing

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -20,7 +20,7 @@ class Landing extends Component {
   componentDidMount() {
     this.effect = window.VANTA.NET({
       el: "#net",
-      color: 0x00FFFF,
+      color: 0x00ffff,
       backgroundColor: 0x3155ac,
       points: 6.0,
       maxDistance: 18.0
@@ -29,10 +29,11 @@ class Landing extends Component {
 
   renderIcons() {
     const { width } = this.state;
-    const isMobile = width <= 500;
+    const isMobile = width <= 700;
     if (isMobile)
       return (
         <div className="social-links-mobile">
+          {console.log("Mobile View")}
           <a
             href="https://lh3.googleusercontent.com/O-sQA7I5Kz5jml5rbYz9TJ0WGMiy7doRXgtsXLiC4DyMLcgyuQxC9aFsGGLds54eFPuYAJGvOScgnH0zo3lpialP2FjztL82aQjuyJVoWFQP9IX0rFOgKgBH63lAPgPrCUIcfITYQ9qIkCMZ3drq35zl52-YbWF5a05FuIhuU2L07HLcvh-kJqzj3Q-12qNuDsXKL8dHgdOOiGgHpXwgMN12inXUePE1tGdXcDUgvNN8jCdurC0rArpw58a0oIC6xd8FX5OCfHmNdXY7qbT0ZQF0VZ-Ls1XG3A7_F4arE7Ij-cFRWrZYmCD0880tsntYo4l_HigDHrahEmTH9GEfY8zFE9VlQwBnsCMA4s1TWAR902A5oc-FzNjfH5j4a2LRlehHfL09F6VIkPrTVAB8_KDSv8ME2XUv9NL75kw6jqPCemsVWFB5jVBmythw0zkmeM2Ct7RREPGbgXfUbbEA5AF9j7Uru_H3KXtttJ1GHGku1sNXeluegHcqOMHZwOcDKaDqDO8aAW0v_cQX_Bzdwz-IOCxhPsWfft4ZYsTl1Rg9QZV6Z9rFw6hUAuknmbH5iF9Wx0k0USRyt6GTXlb-DbE1re2jXqqFgFzkfmvws--cgZQPYDiUjCeB4d2ds0ClUhcjCPWBiCVBQghyi5KDa2wkKbuHuw_NGaJtn1vSY8EXkL5wzMWhhkW0=w2316-h1736-no"
             rel="noopener noreferrer"
@@ -69,6 +70,7 @@ class Landing extends Component {
 
     return (
       <div className="social-links">
+        {console.log("Desktop View")}
         <a
           href="https://lh3.googleusercontent.com/O-sQA7I5Kz5jml5rbYz9TJ0WGMiy7doRXgtsXLiC4DyMLcgyuQxC9aFsGGLds54eFPuYAJGvOScgnH0zo3lpialP2FjztL82aQjuyJVoWFQP9IX0rFOgKgBH63lAPgPrCUIcfITYQ9qIkCMZ3drq35zl52-YbWF5a05FuIhuU2L07HLcvh-kJqzj3Q-12qNuDsXKL8dHgdOOiGgHpXwgMN12inXUePE1tGdXcDUgvNN8jCdurC0rArpw58a0oIC6xd8FX5OCfHmNdXY7qbT0ZQF0VZ-Ls1XG3A7_F4arE7Ij-cFRWrZYmCD0880tsntYo4l_HigDHrahEmTH9GEfY8zFE9VlQwBnsCMA4s1TWAR902A5oc-FzNjfH5j4a2LRlehHfL09F6VIkPrTVAB8_KDSv8ME2XUv9NL75kw6jqPCemsVWFB5jVBmythw0zkmeM2Ct7RREPGbgXfUbbEA5AF9j7Uru_H3KXtttJ1GHGku1sNXeluegHcqOMHZwOcDKaDqDO8aAW0v_cQX_Bzdwz-IOCxhPsWfft4ZYsTl1Rg9QZV6Z9rFw6hUAuknmbH5iF9Wx0k0USRyt6GTXlb-DbE1re2jXqqFgFzkfmvws--cgZQPYDiUjCeB4d2ds0ClUhcjCPWBiCVBQghyi5KDa2wkKbuHuw_NGaJtn1vSY8EXkL5wzMWhhkW0=w2316-h1736-no"
           rel="noopener noreferrer"
@@ -110,11 +112,14 @@ class Landing extends Component {
   }
   render() {
     return (
-      <div  id="net" className="landingBG">
-        <div style={{ width: "100%", margin: "auto" }} >
-          <Grid className="landing-grid" >
-         
-            <Cell style={{marginTop:"5%", paddingTop:"0"}} phone={12} col={12}>
+      <div id="net" className="landingBG">
+        <div style={{ width: "100%", margin: "auto" }}>
+          <Grid className="landing-grid">
+            <Cell
+              style={{ marginTop: "5%", paddingTop: "0" }}
+              phone={12}
+              col={12}
+            >
               <div className="banner-text">
                 <div style={{ textAlign: "center" }}>
                   <img
@@ -127,13 +132,10 @@ class Landing extends Component {
                   <p className="banner-text-p">
                     who enjoys building Android and web apps.
                   </p>
-                  <Cell style={{marginTop:"2.5%"}} col={12} phone={3}>
-              {this.renderIcons()}
-            </Cell>
+                  <div style={{ marginTop: "2.5%" }}>{this.renderIcons()}</div>
                 </div>
               </div>
             </Cell>
-            
           </Grid>
         </div>
       </div>
