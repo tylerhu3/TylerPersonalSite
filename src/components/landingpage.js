cf6f0a2ce57f99bac5727f7f44f8dc1b354703ff
qhu27 <qhu27@ucsc.edu>
2019-09-27 02:29:36 -0700

    diploma linked changed 2

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -19,6 +19,7 @@ size.
 
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
+
 class Landing extends Component {
   constructor() {
     super();
@@ -108,7 +109,7 @@ class Landing extends Component {
         {console.log("Desktop View")}
         <a
         title="Diploma"
-          href="https://lh3.googleusercontent.com/O-sQA7I5Kz5jml5rbYz9TJ0WGMiy7doRXgtsXLiC4DyMLcgyuQxC9aFsGGLds54eFPuYAJGvOScgnH0zo3lpialP2FjztL82aQjuyJVoWFQP9IX0rFOgKgBH63lAPgPrCUIcfITYQ9qIkCMZ3drq35zl52-YbWF5a05FuIhuU2L07HLcvh-kJqzj3Q-12qNuDsXKL8dHgdOOiGgHpXwgMN12inXUePE1tGdXcDUgvNN8jCdurC0rArpw58a0oIC6xd8FX5OCfHmNdXY7qbT0ZQF0VZ-Ls1XG3A7_F4arE7Ij-cFRWrZYmCD0880tsntYo4l_HigDHrahEmTH9GEfY8zFE9VlQwBnsCMA4s1TWAR902A5oc-FzNjfH5j4a2LRlehHfL09F6VIkPrTVAB8_KDSv8ME2XUv9NL75kw6jqPCemsVWFB5jVBmythw0zkmeM2Ct7RREPGbgXfUbbEA5AF9j7Uru_H3KXtttJ1GHGku1sNXeluegHcqOMHZwOcDKaDqDO8aAW0v_cQX_Bzdwz-IOCxhPsWfft4ZYsTl1Rg9QZV6Z9rFw6hUAuknmbH5iF9Wx0k0USRyt6GTXlb-DbE1re2jXqqFgFzkfmvws--cgZQPYDiUjCeB4d2ds0ClUhcjCPWBiCVBQghyi5KDa2wkKbuHuw_NGaJtn1vSY8EXkL5wzMWhhkW0=w2316-h1736-no"
+          href="https://www.dropbox.com/s/3i262b2tke5h9lp/bsDegree.jpg?dl=0"
           rel="noopener noreferrer"
           target="_blank"
         >
